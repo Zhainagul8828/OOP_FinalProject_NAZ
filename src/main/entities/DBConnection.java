a5417db0edf2108a1af88ed0b31f0016476fa51f
Zhainagul Zhumanazar <zhainagul.zhumanazar_2023@UCA.corp>
2021-05-29 16:54:48 +0600

    Initial commit

---
+++ src/main/entities/DBConnection.java
@@ -0,0 +1,18 @@
+package main.entities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+//THIS CLASS IS USED TO RETURN THE DRIVER CONNECTING THE MYSQL DATABASE
+
+public class DBConnection {
+    private static final String USERNAME = "dbuser";
+    private static final String PASSWORD = "dbpassword";
+    private static final String CONN = "jdbc:mysql://localhost/hospital";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(CONN, USERNAME, PASSWORD);
+    }
+}
